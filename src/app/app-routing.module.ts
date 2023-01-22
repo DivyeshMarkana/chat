b517908c9f52b73a8da0c398f3a07513f597f502
Divyesh Markana <divyeshmarkana724@gmail.com>
2023-01-23 03:00:10 +0530

    Inital commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './components/chat/chat.component';
+import { GroupChatComponent } from './components/group-chat/group-chat.component';
+import { UserChatComponent } from './components/user-chat/user-chat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'chat', pathMatch: 'full'
+  },
+  {
+    path: 'chat', component: UserChatComponent
+  },
+  {
+    path: 'group', component: GroupChatComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserChatComponent } from './components/user-chat/user-chat.component';
+import { GroupChatComponent } from './components/group-chat/group-chat.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserChatComponent,
+    GroupChatComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
